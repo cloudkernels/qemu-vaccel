cbf2531c67d3709031a9df4dd0a72874f2f65293
kpapazaf <kpapazaf@cslab.ece.ntua.gr>
2017-12-01 18:40:33 +0200

    Integrate virtio-accel
    Add support for the virtio-accel kernel module and a vaccel backend
    
    Signed-off-by: Kostis Papazafeiropoulos <papazof@gmail.com>

---
+++ include/standard-headers/linux/virtio_accel.h
@@ -0,0 +1,55 @@
+#ifndef _VIRTIO_ACCEL_H
+#define _VIRTIO_ACCEL_H
+
+#include "standard-headers/linux/types.h"
+#include "standard-headers/linux/virtio_types.h"
+
+#define VIRTIO_ID_ACCEL 21
+
+#define VIRTIO_ACCEL_S_HW_READY  (1 << 0)
+
+/* status */
+#define VIRTIO_ACCEL_OK        0
+#define VIRTIO_ACCEL_ERR       1
+#define VIRTIO_ACCEL_BADMSG    2
+#define VIRTIO_ACCEL_NOTSUPP   3
+#define VIRTIO_ACCEL_INVSESS   4 /* Invalid session id */
+
+struct virtio_accel_arg {
+    uint32_t len;
+    unsigned char *buf;
+    unsigned char *usr_buf;
+    unsigned char *usr_pages;
+    uint32_t usr_npages;
+    unsigned char padding[5];
+};
+
+struct virtio_accel_op {
+    uint32_t in_nr;
+    uint32_t out_nr;
+    struct virtio_accel_arg *in;
+    struct virtio_accel_arg *out;
+};
+
+struct virtio_accel_hdr {
+    uint32_t sess_id;
+
+#define VIRTIO_ACCEL_NO_OP                   0
+#define VIRTIO_ACCEL_CREATE_SESSION          1
+#define VIRTIO_ACCEL_DESTROY_SESSION         2
+#define VIRTIO_ACCEL_DO_OP                   3
+    uint32_t op_type;
+
+    /* session create structs */
+    struct virtio_accel_op op;
+};
+
+struct virtio_accel_conf {
+    uint32_t status;
+    /* Supported service mask */
+    uint32_t services;
+    /* Maximum size of each crypto request's content */
+    uint64_t max_size;
+};
+
+#endif /* _VIRTIO_ACCEL_H */
---
+++ include/system/acceldev.h
@@ -0,0 +1,222 @@
+#ifndef ACCELDEV_H
+#define ACCELDEV_H
+
+#include "qemu/queue.h"
+#include "qom/object.h"
+
+#define TYPE_ACCELDEV_BACKEND "acceldev-backend"
+
+OBJECT_DECLARE_TYPE(AccelDevBackend, AccelDevBackendClass,
+                    ACCELDEV_BACKEND)
+
+
+#define MAX_ACCEL_QUEUE_NUM  64
+
+typedef struct AccelDevBackendConf AccelDevBackendConf;
+typedef struct AccelDevBackendPeers AccelDevBackendPeers;
+typedef struct AccelDevBackendClient AccelDevBackendClient;
+typedef struct AccelDevBackend AccelDevBackend;
+
+typedef struct AccelDevBackendArg {
+    uint8_t *buf;
+    uint32_t len;
+} AccelDevBackendArg;
+
+typedef struct AccelDevBackendInfo {
+    uint32_t in_nr;
+    uint32_t out_nr;
+    AccelDevBackendArg *in;
+    AccelDevBackendArg *out;
+} AccelDevBackendInfo;
+
+typedef struct AccelDevBackendSessionInfo {
+    uint32_t op_type;
+    AccelDevBackendInfo op;
+} AccelDevBackendSessionInfo;
+
+typedef struct AccelDevBackendOpInfo {
+    uint32_t op_type;
+    uint32_t sess_id;
+    AccelDevBackendInfo op;
+} AccelDevBackendOpInfo;
+
+
+typedef struct AccelDevBackendClass {
+    ObjectClass parent_class;
+
+    void (*init)(AccelDevBackend *ab, Error **errp);
+    void (*cleanup)(AccelDevBackend *ab, Error **errp);
+
+    int64_t (*create_session)(AccelDevBackend *ab,
+                       AccelDevBackendSessionInfo *sess_info,
+                       uint32_t queue_index, Error **errp);
+    int (*destroy_session)(AccelDevBackend *ab,
+                           uint32_t session_id,
+                           uint32_t queue_index, Error **errp);
+    int (*do_op)(AccelDevBackend *ab,
+                     AccelDevBackendOpInfo *op_info,
+                     uint32_t queue_index, Error **errp);
+} AccelDevBackendClass;
+
+
+struct AccelDevBackendClient {
+    char *model;
+    char *name;
+    char *info_str;
+    unsigned int queue_index;
+    QTAILQ_ENTRY(AccelDevBackendClient) next;
+};
+
+struct AccelDevBackendPeers {
+    AccelDevBackendClient *ccs[MAX_ACCEL_QUEUE_NUM];
+    uint32_t queues;
+};
+
+struct AccelDevBackendConf {
+    AccelDevBackendPeers peers;
+
+    /* Supported service mask */
+    uint32_t services;
+    /* Maximum size of each accel request's content */
+    uint64_t max_size;
+};
+
+struct AccelDevBackend {
+    Object parent_obj;
+
+    bool ready;
+    /* Tag the acceldev backend is used by virtio-accel or not */
+    bool is_used;
+    AccelDevBackendConf conf;
+};
+
+/**
+ * acceldev_backend_new_client:
+ * @model: the acceldev backend model
+ * @name: the acceldev backend name, can be NULL
+ *
+ * Creates a new acceldev backend client object
+ * with the @name in the model @model.
+ *
+ * The returned object must be released with
+ * acceldev_backend_free_client() when no
+ * longer required
+ *
+ * Returns: a new acceldev backend client object
+ */
+AccelDevBackendClient *
+acceldev_backend_new_client(const char *model, const char *name);
+
+/**
+ * acceldev_backend_free_client:
+ * @cc: the acceldev backend client object
+ *
+ * Release the memory associated with @cc that
+ * was previously allocated by acceldev_backend_new_client()
+ */
+void acceldev_backend_free_client(AccelDevBackendClient *c);
+
+/**
+ * acceldev_backend_cleanup:
+ * @backend: the acceldev backend object
+ * @errp: pointer to a NULL-initialized error object
+ *
+ * Clean the resouce associated with @backend that realizaed
+ * by the specific backend's init() callback
+ */
+void acceldev_backend_cleanup(
+           AccelDevBackend *ab,
+           Error **errp);
+
+/**
+ * acceldev_backend_sym_create_session:
+ * @backend: the acceldev backend object
+ * @sess_info: parameters needed by session creating
+ * @queue_index: queue index of acceldev backend client
+ * @errp: pointer to a NULL-initialized error object
+ *
+ * Create a session for symmetric algorithms
+ *
+ * Returns: session id on success, or -1 on error
+ */
+int64_t acceldev_backend_create_session(
+           AccelDevBackend *ab,
+           AccelDevBackendSessionInfo *sess_info,
+           uint32_t queue_index, Error **errp);
+
+/**
+ * acceldev_backend_sym_destroy_session:
+ * @backend: the acceldev backend object
+ * @session_id: the session id
+ * @queue_index: queue index of acceldev backend client
+ * @errp: pointer to a NULL-initialized error object
+ *
+ * Close a session for symmetric algorithms which was previously
+ * created by acceldev_backend_sym_create_session()
+ *
+ * Returns: 0 on success, or Negative on error
+ */
+int acceldev_backend_destroy_session(
+           AccelDevBackend *ab,
+           uint64_t session_id,
+           uint32_t queue_index, Error **errp);
+
+/**
+ * acceldev_backend_crypto_operation:
+ * @backend: the acceldev backend object
+ * @opaque: pointer to a VirtIOCryptoReq object
+ * @queue_index: queue index of acceldev backend client
+ * @errp: pointer to a NULL-initialized error object
+ *
+ * Do crypto operation, such as encryption and
+ * decryption
+ *
+ * Returns: VIRTIO_CRYPTO_OK on success,
+ *         or -VIRTIO_CRYPTO_* on error
+ */
+int acceldev_backend_operation(
+                 AccelDevBackend *ab,
+				 AccelDevBackendOpInfo *op_info,
+                 uint32_t queue_index, Error **errp);
+
+/**
+ * acceldev_backend_set_used:
+ * @backend: the acceldev backend object
+ * @used: ture or false
+ *
+ * Set the acceldev backend is used by virtio-crypto or not
+ */
+void acceldev_backend_set_used(AccelDevBackend *ab, bool used);
+
+/**
+ * acceldev_backend_is_used:
+ * @backend: the acceldev backend object
+ *
+ * Return the status that the acceldev backend is used
+ * by virtio-crypto or not
+ *
+ * Returns: true on used, or false on not used
+ */
+bool acceldev_backend_is_used(AccelDevBackend *ab);
+
+/**
+ * acceldev_backend_set_ready:
+ * @backend: the acceldev backend object
+ * @ready: ture or false
+ *
+ * Set the acceldev backend is ready or not, which is called
+ * by the children of the acceldev banckend interface.
+ */
+void acceldev_backend_set_ready(AccelDevBackend *ab, bool ready);
+
+/**
+ * acceldev_backend_is_ready:
+ * @backend: the acceldev backend object
+ *
+ * Return the status that the acceldev backend is ready or not
+ *
+ * Returns: true on ready, or false on not ready
+ */
+bool acceldev_backend_is_ready(AccelDevBackend *ab);
+
+#endif /* ACCELDEV_H */
